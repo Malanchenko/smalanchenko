5fcbfad167a9fa89ef203acc1a48d7550cabe86e
Malan85 <msn85@ukr.net>
2017-12-01 16:36:31 +0000

    1.3. JavaDoc. Code convention

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+/**
+ * Package for calculate task.
+ *
+ * @author Sergii Malanchenko (msn85@ukr.net)
+ * @version $Id$
+ * @since 01.12.2017
+*/
+package ru.job4j;
+
+public class Calculate
+{
+public static void main(String[] args)
+   {
+	System.out.println("Hello World");
+   }
+/**
+ * Вывод строки
+*/
+}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Sergii Malanchenko (msn85@ukr.net)
+ * @version $Id$
+ * @since 01.12.2017
+*/
